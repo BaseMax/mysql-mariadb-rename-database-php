b82e212db5e31b24f59ca0dfbede1d1ce60be671
Max Base <MaxBaseCode@Gmail.Com>
2020-12-26 09:39:52 +0330

    Adding global vars and open db connection

--- rename.php
+++ rename.php
@@ -14,3 +14,12 @@ $dbname = "stock";
 $nextDbname= "newDB";
 $dbname = "newDB";
 $nextDbname= "stock";
+
+// Global variables
+$tables = [];
+
+// Open
+$mysqli = new mysqli($servername, $username, $password, $dbname);
+if($mysqli->connect_error) {
+  die("Connection failed: " . $mysqli->connect_error);
+}
