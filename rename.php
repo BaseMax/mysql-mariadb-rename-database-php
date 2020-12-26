43795962f9966279f0d825b4b1f418688dc6c06f
Max Base <MaxBaseCode@Gmail.Com>
2020-12-26 09:39:25 +0330

    Update rename.php

--- rename.php
+++ rename.php
@@ -1,1 +1,7 @@
-
+<?php
+/*
+ * @Name: Mysql Mariadb Rename Database PHP
+ * @Author: Max Base
+ * @Date: 2020-12-26
+ * @Repository: https://github.com/basemax/mysql-mariadb-rename-database-php
+ */
